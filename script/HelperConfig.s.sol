79480ae8573d67f3c6ec87b659190cf9e9b6d237
AkshatOdiya <akshatodiya79@gmail.com>
2025-06-23 17:00:27 +0530

    Updated HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {VRFCoordinatorV2PlusMock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2PlusMock.sol";
+import {VRFCoordinatorV2PlusMock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2PlusMock.sol";
 /**
  * @dev This mock link token Contract from Patrick uses dependency ERC20 Contract
  * It can be installed using  `forge install transmissions11/solmate`
@@ -43,7 +43,8 @@ contract HelperConfig is Script, constants {
         uint256 deployerKey;
     }
 
-    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 public constant DEFAULT_ANVIL_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfig;
@@ -52,7 +53,9 @@ contract HelperConfig is Script, constants {
         networkConfig[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
     }
 
-    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
         // ChainIB of ETH sepolia is 1115511, you can see on Chainlist by typing ethereum sepolia
         // Verifying that a VRF coordinator exists
         if (networkConfig[chainId].vrfCoordinator != address(0)) {
@@ -90,7 +93,10 @@ contract HelperConfig is Script, constants {
             return localNetworkConfig;
         }
         vm.startBroadcast();
-        VRFCoordinatorV2PlusMock vrfCoordinatorMock = new VRFCoordinatorV2PlusMock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK);
+        VRFCoordinatorV2PlusMock vrfCoordinatorMock = new VRFCoordinatorV2PlusMock(
+                MOCK_BASE_FEE,
+                MOCK_GAS_PRICE_LINK
+            );
         LinkToken linkToken = new LinkToken();
 
         vm.stopBroadcast();
